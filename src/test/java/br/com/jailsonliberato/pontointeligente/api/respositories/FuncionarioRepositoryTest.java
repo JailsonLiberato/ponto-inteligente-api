6acc08bd1cdb6638fb1eb866f0d7c01438be43b0
Jailson Liberato <jailsonliberato@gmail.com>
2019-07-04 13:09:23 -0300

    Adicionando os testes de FuncionarioRepository e LancamentoRepository.

--- src/test/java/br/com/jailsonliberato/pontointeligente/api/respositories/FuncionarioRepositoryTest.java
+++ src/test/java/br/com/jailsonliberato/pontointeligente/api/respositories/FuncionarioRepositoryTest.java
@@ -1,5 +1,89 @@
 package br.com.jailsonliberato.pontointeligente.api.respositories;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.math.BigDecimal;
+import java.security.NoSuchAlgorithmException;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import br.com.jailsonliberato.pontointeligente.api.entities.Empresa;
+import br.com.jailsonliberato.pontointeligente.api.entities.Funcionario;
+import br.com.jailsonliberato.pontointeligente.api.enums.PerfilEnum;
+import br.com.jailsonliberato.pontointeligente.api.repositories.EmpresaRepository;
+import br.com.jailsonliberato.pontointeligente.api.repositories.FuncionarioRepository;
+import br.com.jailsonliberato.pontointeligente.api.utils.PasswordUtils;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
 public class FuncionarioRepositoryTest {
+
+	@Autowired
+	private FuncionarioRepository funcionarioRepository;
+
+	@Autowired
+	private EmpresaRepository empresaRepository;
+
+	private static final String EMAIL = "email@email.com";
+	private static final String CPF = "24291173474";
+
+	@Before
+	public void setUp() throws Exception {
+		Empresa empresa = this.empresaRepository.save(obterDadosEmpresa());
+		this.funcionarioRepository.save(obterDadosFuncionario(empresa));
+	}
+	
+	@Test
+	public void testBuscarFuncionarPorEmail() {
+		Funcionario funcionario = this.funcionarioRepository.findByEmail(EMAIL);
+		assertEquals(EMAIL, funcionario.getEmail());
+	}
+	
+	@Test
+	public void testBuscarFuncionarPorCpf() {
+		Funcionario funcionario = this.funcionarioRepository.findByCpf(CPF);
+		assertEquals(CPF, funcionario.getCpf());
+	}
+
+	@Test
+	public void testBuscarFuncionarioPorEmailECpfParaCpfInvalido() {
+		Funcionario funcionario = this.funcionarioRepository.findByCpfOrEmail("12345678901", EMAIL);
+		assertNotNull(funcionario);
+	}
+
+	private Funcionario obterDadosFuncionario(Empresa empresa) throws NoSuchAlgorithmException {
+		Funcionario funcionario = new Funcionario();
+		funcionario.setNome("Fulano de tal");
+		funcionario.setPerfil(PerfilEnum.ROLE_USUARIO);
+		funcionario.setSenha(PasswordUtils.gerarBCrypt("123456"));
+		funcionario.setCpf(CPF);
+		funcionario.setEmail(EMAIL);
+		funcionario.setQtdHorasAlmoco(1.0f);
+		funcionario.setQtdHorasTrabalhoDia(8.0f);
+		funcionario.setEmpresa(empresa);
+		funcionario.setValorHora(BigDecimal.ONE);
+		return funcionario;
+	}
+
+	private Empresa obterDadosEmpresa() {
+		Empresa empresa = new Empresa();
+		empresa.setRazaoSocial("Empresa de exemplo");
+		empresa.setCnpj("51463645000100");
+		return empresa;
+	}
+
+	@After
+	public final void tearDown() {
+		this.empresaRepository.deleteAll();
+	}
 
 }
--- src/test/java/br/com/jailsonliberato/pontointeligente/api/respositories/LancamentoRepositoryTest.java
+++ src/test/java/br/com/jailsonliberato/pontointeligente/api/respositories/LancamentoRepositoryTest.java
@@ -1,5 +1,105 @@
 package br.com.jailsonliberato.pontointeligente.api.respositories;
 
+import static org.junit.Assert.assertEquals;
+
+import java.math.BigDecimal;
+import java.security.NoSuchAlgorithmException;
+import java.util.Date;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import br.com.jailsonliberato.pontointeligente.api.entities.Empresa;
+import br.com.jailsonliberato.pontointeligente.api.entities.Funcionario;
+import br.com.jailsonliberato.pontointeligente.api.entities.Lancamento;
+import br.com.jailsonliberato.pontointeligente.api.enums.PerfilEnum;
+import br.com.jailsonliberato.pontointeligente.api.enums.TipoEnum;
+import br.com.jailsonliberato.pontointeligente.api.repositories.EmpresaRepository;
+import br.com.jailsonliberato.pontointeligente.api.repositories.FuncionarioRepository;
+import br.com.jailsonliberato.pontointeligente.api.repositories.LancamentoRepository;
+import br.com.jailsonliberato.pontointeligente.api.utils.PasswordUtils;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
 public class LancamentoRepositoryTest {
+
+	@Autowired
+	private LancamentoRepository lancamentoRepository;
+
+	@Autowired
+	private FuncionarioRepository funcionarioRepository;
+
+	@Autowired
+	private EmpresaRepository empresaRepository;
+
+	private Long funcionarioId;
+
+	@Before
+	public void setUp() throws Exception {
+		Empresa empresa = this.empresaRepository.save(obterDadosEmpresa());
+		Funcionario funcionario = this.funcionarioRepository.save(obterDadosFuncionario(empresa));
+		this.funcionarioId = funcionario.getId();
+		this.lancamentoRepository.save(obterDadosLancamento(funcionario));
+		this.lancamentoRepository.save(obterDadosLancamento(funcionario));
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		this.empresaRepository.deleteAll();
+	}
+
+	@Test
+	public void testBuscarLancamentosPorFuncionarioId() {
+		List<Lancamento> lancamentos = this.lancamentoRepository.findByFuncionarioId(funcionarioId);
+		assertEquals(2, lancamentos.size());
+	}
+
+	@Test
+	public void testBuscarLancamentosPorFuncionarioIdPaginado() {
+		PageRequest page = new PageRequest(0, 10);
+		Page<Lancamento> lancamentos = this.lancamentoRepository.findByFuncionarioId(funcionarioId, page);
+		assertEquals(2, lancamentos.getTotalElements());
+	}
+
+	private Funcionario obterDadosFuncionario(Empresa empresa) throws NoSuchAlgorithmException {
+		Funcionario funcionario = new Funcionario();
+		funcionario.setNome("Fulano de tal");
+		funcionario.setPerfil(PerfilEnum.ROLE_USUARIO);
+		funcionario.setSenha(PasswordUtils.gerarBCrypt("123456"));
+		funcionario.setCpf("24291173474");
+		funcionario.setEmail("email@email.com");
+		funcionario.setQtdHorasAlmoco(1.0f);
+		funcionario.setQtdHorasTrabalhoDia(8.0f);
+		funcionario.setEmpresa(empresa);
+		funcionario.setValorHora(BigDecimal.ONE);
+		return funcionario;
+	}
+
+	private Empresa obterDadosEmpresa() {
+		Empresa empresa = new Empresa();
+		empresa.setRazaoSocial("Empresa de exemplo");
+		empresa.setCnpj("51463645000100");
+		return empresa;
+	}
+
+	private Lancamento obterDadosLancamento(Funcionario funcionario) {
+		Lancamento lancamento = new Lancamento();
+		lancamento.setData(new Date());
+		lancamento.setTipo(TipoEnum.INICIO_ALMOCO);
+		lancamento.setDescricao("Teste");
+		lancamento.setLocalizacao("165465545454");
+		lancamento.setFuncionario(funcionario);
+		return lancamento;
+	}
 
 }
